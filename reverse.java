8913f06c0d536fb11beff63024d3cb0fee145161
Asha2805 <43291441+Asha2805@users.noreply.github.com>
2018-09-27 11:43:06 +0530

    Update and rename README.md to reverse.java

---
+++ reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class reverse
+{
+public static void main(String[] args)
+{
+String input;
+Scanner w=new Scanner(System.in);
+input=w.nextLine();
+StringBuilder input1=new StringBuilder();
+input1=append.input();
+input1=input1.reverse();
+system.out.println(input1);
+}
+}
